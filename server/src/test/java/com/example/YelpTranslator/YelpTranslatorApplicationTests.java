e9a4b4e14c6bd3d98b85e1c43b0d5ac3d4dfeb4d
Sania Pervez <139937275+sanipervez@users.noreply.github.com>
2025-12-09 22:40:14 -0500

    initial setup with java spring framework.

---
+++ server/src/test/java/com/example/YelpTranslator/YelpTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YelpTranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
